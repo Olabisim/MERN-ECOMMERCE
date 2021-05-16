6067c89befb8c3986a133e8736479472c1534f85
olabisi <ajoseholabisi@gmail.com>
2021-05-16 21:49:37 +0100

    react-setup

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+
+const App = () => {
+
+    return (
+        <>
+            <h1>Welcome to ProShop</h1>
+        </>
+    );
+    
+}
+
+export default App;
