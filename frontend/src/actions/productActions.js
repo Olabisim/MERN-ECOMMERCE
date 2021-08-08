e4fa37c87dab1b9950f6e0223fa117f6365232ac
olabisi <ajoseholabisi@gmail.com>
2021-08-08 23:45:26 +0100

    create and update product endpoints

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,15 +1,24 @@
 import axios from 'axios'
 
 import {
+
     PRODUCT_LIST_REQUEST,
     PRODUCT_LIST_SUCCESS,
     PRODUCT_LIST_FAILURE,
+
     PRODUCT_DETAILS_REQUEST,
     PRODUCT_DETAILS_SUCCESS,
     PRODUCT_DETAILS_FAILURE,
+
     PRODUCT_DELETE_REQUEST,
     PRODUCT_DELETE_SUCCESS,
     PRODUCT_DELETE_FAILURE,
+
+    PRODUCT_CREATE_REQUEST,
+    PRODUCT_CREATE_SUCCESS,
+    PRODUCT_CREATE_FAILURE,
+    PRODUCT_CREATE_RESET,
+
 } from "../constants/productConstants";
 
 export const listProducts = () => async (dispatch) => {
@@ -86,6 +95,48 @@ export const deleteProductAction = (id) => async (dispatch, getState) => {
         dispatch({
 
             type: PRODUCT_DELETE_FAILURE,
+            payload:  
+                error.response && error.response.data.message
+                ? error.response.data.message
+                : error.message,
+
+        })
+    }
+
+}
+
+
+export const createProductAction = () => async (dispatch, getState) => {
+    
+    try {
+        dispatch({
+            type: PRODUCT_CREATE_REQUEST,
+        })
+
+        const {
+            userLogin: {userInfo},
+        } = getState()
+
+        const config = {
+            headers: {
+                // 'Content-Type': 'application/json',
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        }
+        
+        const { data } = await axios.post(`/api/products`, {}, config)
+
+        dispatch({
+            type: PRODUCT_CREATE_SUCCESS,
+            payload: data
+
+        })
+
+    }
+    catch (error) {
+        dispatch({
+
+            type: PRODUCT_CREATE_FAILURE,
             payload:  
                 error.response && error.response.data.message
                 ? error.response.data.message
--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,13 +1,22 @@
 import {
+
     PRODUCT_LIST_REQUEST,
     PRODUCT_LIST_SUCCESS,
     PRODUCT_LIST_FAILURE,
+
     PRODUCT_DETAILS_REQUEST,
     PRODUCT_DETAILS_SUCCESS,
     PRODUCT_DETAILS_FAILURE,
+
     PRODUCT_DELETE_REQUEST,
     PRODUCT_DELETE_SUCCESS,
     PRODUCT_DELETE_FAILURE,
+
+    PRODUCT_CREATE_REQUEST,
+    PRODUCT_CREATE_SUCCESS,
+    PRODUCT_CREATE_FAILURE,
+    PRODUCT_CREATE_RESET,
+
 } from "../constants/productConstants";
 
 export const productListReducer = (state = { products: [] }, action) => {
@@ -56,6 +65,30 @@ export const productDeleteReducer = (state = {}, action) => {
 
         case PRODUCT_DELETE_FAILURE:
             return { loading: false, error: action.payload };
+
+        default:
+            return state;
+
+    }
+
+};
+
+
+export const productCreateReducer = (state = {}, action) => {
+
+    switch (action.type) {
+
+        case PRODUCT_CREATE_REQUEST:
+            return { loading: true };
+
+        case PRODUCT_CREATE_SUCCESS:
+            return { loading: false, success: true, product: action.payload };
+
+        case PRODUCT_CREATE_FAILURE:
+            return { loading: false, error: action.payload };
+
+        case PRODUCT_CREATE_RESET:
+            return { };
 
         default:
             return state;
