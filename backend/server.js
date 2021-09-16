2ea809092e6371e1930c98f6cb169d90b18e97e2
olabisi <ajoseholabisi@gmail.com>
2021-09-16 19:36:03 +0100

    solving {'message':'Not Found - /','stack':null}

--- backend/server.js
+++ backend/server.js
@@ -31,10 +31,6 @@ connectDB()
 
 const app = express();
 
-if(process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'))
-}
-
 
 app.use(express.json())
 
@@ -51,6 +47,21 @@ app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_I
 // then path.resolve we use to solve that
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+if(process.env.NODE_ENV === 'production') {
+
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    })
+
+}
+else {
+    app.get('/', (req, res) => {
+        res.send('API is running...');
+    })
+}
 
 
 app.use(notFound)
